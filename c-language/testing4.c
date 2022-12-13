88302d295f492e13e6f5c6aec8eec73255b956b1
Raghavendra-varma-au-cse <113962167+Raghavendra-varma-au-cse@users.noreply.github.com>
2022-12-13 22:02:29 +0530

    To calculate the roots of the Quadratic equations
    This is just the initial code and it required many change to make it more accurate and faster

---
+++ c-language/testing4.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int x,a,b,c,D,X,Y;
+    printf("To Calculate the roots of the given quadratic equation");
+    printf("Please Enter a b c values: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if(a != 0){
+        if(b<0){
+            printf("\n\nThe quadratic equation is %dx^2 - %dx + %d = 0\n\n",a,abs(b),c);
+        }
+        else if(c<0){
+            printf("\n\nThe quadratic equation is %dx^2 + %dx - %d = 0\n\n",a,b,abs(c));
+        }
+        else{
+            printf("\n\nThe quadratic equation is %dx^2 - %dx + %d = 0\n\n",a,abs(b),c);
+        }
+        D = b*b - 4*a*c;
+        if(D > 0){
+            printf("This equation has real and distinct roots (Unreal)\n\n");
+            X = (-b+sqrt(b*b - 4*a*c))/2*a;
+            Y = (-b-sqrt(b*b - 4*a*c))/2*a;
+            printf("Roots are (%d,%d)", X, Y);
+        }
+        else if(D < 0){
+            printf("This equation has imaginary and unreal roots\n\n");
+            X = -b/2*a;
+            Y = sqrt(abs(b*b - 4*a*c))/2*a;
+            printf("Roots are (%d + i%d)", X, Y);
+        }
+        else{
+            printf("This equation has real and equal roots\n\n");
+            X = (-b+sqrt(b*b - 4*a*c))/2*a;
+            Y = (-b-sqrt(b*b - 4*a*c))/2*a;
+            printf("Roots are (%d,%d)", X, Y);
+        }
+        printf("\n\n");
+
+    }
+    else{
+        printf("Equation is Invalid (a != 0)");
+    }
+    return 0;
+}
